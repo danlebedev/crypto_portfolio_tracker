e22be26c6d645711f62a881cadaed58a7d8caf95
danlebedev <lebedev.py@gmail.com>
2023-05-05 21:34:12 +0300

    create model Portfolio

--- cptracker/models.py
+++ cptracker/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Portfolio(models.Model):
+    name = models.CharField(max_length=20)
+    balance = models.DecimalField(max_digits=24, decimal_places=12)
+    created = models.DateTimeField(auto_now_add=True, db_index=True)
