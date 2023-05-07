dd56da8d1aff522f74a20e32519a18d51bbffac3
danlebedev <lebedev.py@gmail.com>
2023-05-07 15:33:07 +0300

    rename attribute in Portfolio model

--- cptracker/models.py
+++ cptracker/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class Portfolio(models.Model):
     name = models.CharField(max_length=20)
     balance = models.DecimalField(max_digits=24, decimal_places=12)
-    created = models.DateTimeField(auto_now_add=True, db_index=True)
+    date_added = models.DateTimeField(auto_now_add=True, db_index=True)
