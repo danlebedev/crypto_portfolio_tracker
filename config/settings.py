85a93be726692a5cf27c70a48c98a4ce7446c349
danlebedev <lebedev.py@gmail.com>
2023-05-27 21:59:44 +0300

    create main app & add model Asset

--- config/settings.py
+++ config/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     # My apps.
+    'main',
     'cptracker',
 
     # Django default apps.
---
+++ main/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class Asset(models.Model):
+    name = models.CharField(max_length=20)
+    short_name = models.CharField(max_length=10)
+
+    class AssetTypes(models.IntegerChoices):
+        STOCK = 1, 'stock'
+        BOND = 2, 'bond'
+        CURRENCY = 3, 'currency'
+        COMMODITY = 4, 'commodity'
+        CRYPTOCURRENCY = 5, 'cryptocurrency'
+
+    asset_type = models.SmallIntegerField(
+        choices=AssetTypes.choices,
+        default=AssetTypes.CURRENCY,
+    )
