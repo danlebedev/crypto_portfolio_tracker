43843974f01ac006858f59b8eca481025eea7c18
danlebedev <lebedev.py@gmail.com>
2023-05-29 14:45:28 +0300

    ADD change_asset page

--- main/urls.py
+++ main/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     # Assets
     path('assets/', views.assets, name='assets'),
-    path('assets/<int:asset_id>', views.asset, name='asset'),
+    path('assets/<int:asset_id>/', views.asset, name='asset'),
+    path('assets/<int:pk>/change', views.AssetUpdateView.as_view(), name='change_asset'),
     path('assets/add/', views.AssetCreateView.as_view(), name='add_asset'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,7 @@
+from typing import Any, Dict
+from django.db import models
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from django.urls import reverse_lazy
 
 from . import models
@@ -20,6 +22,17 @@ def asset(request, asset_id):
     asset = models.Asset.objects.get(id=asset_id)
     context = {'asset': asset}
     return render(request, 'main/asset.html', context)
+
+class AssetUpdateView(UpdateView):
+    model = models.Asset
+    template_name = 'main/change_asset.html'
+    form_class = forms.AssetForm
+    success_url = reverse_lazy('main:assets')
+
+    def get_context_data(self, *args, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['assets'] = models.Asset.objects.all()
+        return context
 
 class AssetCreateView(CreateView):
     template_name = 'main/add_asset.html'
