88a6c7d27f144039622c027b4220a7af37624e31
danlebedev <lebedev.py@gmail.com>
2023-05-29 15:02:33 +0300

    ADD change_portfolio page & CHANGE templates

--- cptracker/urls.py
+++ cptracker/urls.py
@@ -10,5 +10,10 @@ urlpatterns = [
     # Portfolios.
     path('portfolios/', views.portfolios, name='portfolios'),
     path('portfolios/<int:portfolio_id>/', views.portfolio, name='portfolio'),
+    path(
+        'portfolios/<int:pk>/change_portfolio',
+        views.PortfolioUpdateView.as_view(),
+        name='change_portfolio'
+    ),
     path('portfolios/add/', views.PortfolioCreateView.as_view(), name='add_portfolio'),
 ]
--- cptracker/views.py
+++ cptracker/views.py
@@ -1,5 +1,6 @@
+from typing import Any, Dict
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from django.urls import reverse_lazy
 
 from . import models
@@ -19,6 +20,17 @@ def portfolio(request, portfolio_id):
     portfolio = models.Portfolio.objects.get(id=portfolio_id)
     context = {'portfolio': portfolio}
     return render(request, 'cptracker/portfolio.html', context)
+
+class PortfolioUpdateView(UpdateView):
+    model = models.Portfolio
+    template_name = 'cptracker/change_portfolio.html'
+    form_class = forms.PortfolioForm
+    success_url = reverse_lazy('cptracker:portfolios')
+
+    def get_context_data(self, *args, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['assets'] = models.Portfolio.objects.all()
+        return context
 
 class PortfolioCreateView(CreateView):
     template_name = 'cptracker/add_portfolio.html'
